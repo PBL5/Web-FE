ac33daf25bec030922803c5cfc016edf681652de
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-04-28 00:06:24 +0700

    fix bug cua con Huyen + add prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import SideEffectClass from './testES6/SideEffectClass';
-import TestHook from './testES6/TestHook';
 
 ReactDOM.render(
   <React.StrictMode>
