9e1dde4cabd6b4933e636e39312aa5a2748ff6ea
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-05-23 16:09:19 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const API_URL = 'http://localhost:8000/api'
+const environment = 'dev';
+// const environment = 'production';
+
+export const API_URL = environment === 'dev' ? 'http://localhost:8000/api' : 'http://http://13.228.21.39/:8000/api';
